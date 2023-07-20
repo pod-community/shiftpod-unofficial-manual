48ec4edd4e914d24d1ff045ee990703b947b87ee
Aravindo Wingeier <wingeier@adobe.com>
2023-07-19 22:44:43 -0700

    init

--- _config.ts
+++ _config.ts
@@ -1,8 +1,9 @@
 import lume from "lume/mod.ts";
 import jsx from "lume/plugins/jsx.ts";
+import wiki from "https://deno.land/x/lume_theme_simple_wiki@v0.1.0/mod.ts";
 
 const site = lume();
-
+site.use(wiki());
 site.use(jsx());
 
 export default site;
