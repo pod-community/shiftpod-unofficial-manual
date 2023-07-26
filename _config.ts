f6ccfc80c63ae4fd353d2a978fbaf776e01a892a
Aravindo Wingeier <wingeier@adobe.com>
2023-07-26 09:12:26 -0700

    sitemap

--- _config.ts
+++ _config.ts
@@ -1,6 +1,7 @@
 import lume from "lume/mod.ts";
 import notes from "./mod.ts";
 import jsx from "lume/plugins/jsx.ts";
+import sitemap from "lume/plugins/sitemap.ts";
 
 const site = lume({
   location: new URL("https://pod-community.github.io/shiftpod-unofficial-manual/"),
@@ -8,5 +9,6 @@ const site = lume({
 site.ignore("README.md", "CHANGELOG.md", "node_modules");
 site.use(notes());
 site.use(jsx());
+site.use(sitemap());
 
 export default site;
